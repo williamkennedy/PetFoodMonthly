4961c4f6e30933733e4c6bfddb01c1659cbb9a5b
Hannah Stephenson <littlehannah96@gmail.com>
2015-01-26 14:01:19 +0000

    configured Stripe in app.rb

--- app.rb
+++ app.rb
@@ -1,10 +1,16 @@
 require "sinatra"
+require "stripe"
 
 configure :development do
   set :bind, '0.0.0.0'
   set :port, 3000
 end
 
-    get '/' do
-        erb :"index", layout: :"layouts/main"
-    end
+set :publishable_key, ENV['PUBLISHABLE_KEY']
+set :secret_key, ENV['SECRET_KEY']
+
+Stripe.api_key = settings.secret_key
+
+get '/' do
+    erb :"index", layout: :"layouts/main"
+end
