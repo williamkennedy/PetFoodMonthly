7082ddbfdee11d11a6209573e294f8c7a816f58a
Hannah Stephenson <littlehannah96@gmail.com>
2015-01-26 14:16:20 +0000

    tidied up app.rb

--- app.rb
+++ app.rb
@@ -6,14 +6,15 @@ configure :development do
   set :port, 3000
 end
 
+get '/' do
+    erb :"index", layout: :"layouts/main"
+end
+
+# ---- Stripe Configuration --- #
 set :publishable_key, ENV['PUBLISHABLE_KEY']
 set :secret_key, ENV['SECRET_KEY']
 
 Stripe.api_key = settings.secret_key
-
-get '/' do
-    erb :"index", layout: :"layouts/main"
-end
 
 post '/charge' do
 	# Amount in cents
