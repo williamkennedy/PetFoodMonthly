90c3f399f530dd0f9725ce6d0bd4ecb9dc3d9a70
Hannah Stephenson <littlehannah96@gmail.com>
2015-02-02 21:10:43 +0000

    Configured Active Directory with Sqlite

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require "sinatra"
+require "active_record"
 require "stripe"
 
 configure :development do
@@ -6,8 +7,16 @@ configure :development do
   set :port, 3000
 end
 
+class Article < ActiveRecord::Base
+end
+
 get '/' do
     erb :"index", layout: :"layouts/main"
+    Article.establish_connection(
+    	:adapter => "sqlite3",
+    	:database => "hw.db"
+    )
+    Article.first.title
 end
 
 # ---- Stripe Configuration --- #
